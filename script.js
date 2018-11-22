1437ab3ce6ebdf575ccabaa0acff4fa448636e3f
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2018-11-22 20:50:02 +1100

    Update script.js

--- script.js
+++ script.js
@@ -15,5 +15,5 @@ $(document).ready(function() {
     var timer = setInterval(changeFirefly, 500);
 });
 function changeFirefly() {
-    $("#firefly_test").css("background", "rgba(255, 245, 100, " + Math.random());
+    $("#firefly_test").css("background", "rgba(253, 239, 195, " + Math.random());
 }
