39823bf3cd042e30963268665f812f92179d56a8
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2018-11-22 15:48:26 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-$("#hamburger").click(function() {
+$("#top_hamburger").click(function() {
     alert("this works");
     if ($("#top_menu").is(":visible")) {
         $("#top_menu").css("display", "none");
-        $("#hamburger").css("transform", "rotate(90deg)");
+        $("#top_hamburger").css("transform", "rotate(90deg)");
     } else {
         $("#top_menu").css("display", "block");
-        $("#hamburger").css("transform", "rotate(0deg)");
+        $("#top_hamburger").css("transform", "rotate(0deg)");
     }
 });
