e09c14ab03b095c10056d541afe5545a820586da
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2018-11-22 17:55:09 +1100

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ $(document).ready(function() {
             $("#top_hamburger").css("transform", "rotate(90deg)");
         }
     });
-    var timer = setTimeout(function() {
-        $("#firefly_test").css("background", "rgba(230, 220, 10, " + Math.random());
-    }, 500);
+    var timer = setTimeout(changeFirefly, 500);
 });
+function changeFirefly {
+    $("#firefly_test").css("background", "rgba(230, 220, 10, " + Math.random());
+}
