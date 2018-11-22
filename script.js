bb1f180961085108bc12a241cc23abb117ca8cb4
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2018-11-22 15:47:34 +1100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $("#hamburger").click(function() {
         $("#top_menu").css("display", "none");
         $("#hamburger").css("transform", "rotate(90deg)");
     } else {
-        $("#top_menu").css("display", "inline-block");
+        $("#top_menu").css("display", "block");
         $("#hamburger").css("transform", "rotate(0deg)");
     }
 });
