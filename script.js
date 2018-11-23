8edbccd8d8b9d26c35ebd0a01170e7471a71bc67
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2018-11-23 13:43:30 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     $("#top_hamburger").click(function() {
         if ($("#top_menu").height() > 0) {
-            $("#top_menu li").css("font-size", "0px");
+            $("#top_menu li").css("font-size", "0pt");
             $("#top_menu li").css("color", "transparent");
             $("#top_hamburger").css("transform", "rotate(0deg)");
         } else {
-            $("#top_menu li").css("font-size", "20px");
+            $("#top_menu li").css("font-size", "20pt");
             $("#top_menu li").css("color", "#fff");
             $("#top_hamburger").css("transform", "rotate(90deg)");
         }
