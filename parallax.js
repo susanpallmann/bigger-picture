6b2c9445b164782a0979f93f7525245dcfad301f
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2018-11-24 00:33:47 +1100

    Update parallax.js

--- parallax.js
+++ parallax.js
@@ -1,3 +1,8 @@
 window.addEventListener("scroll", function(e){
-
+  // for all containers ("animal" class), get scroll position where:
+  // 0% = when container first enters bottom of screen (i.e. when distance scrolled = distance from top of document
+  // 100% = when container first leaves top of screen (i.e. when distance scrolled = distance from top of document + container height + viewport height)\
+  // should be similar to svg code?
+  // for all children... check class type
+  // if class = "layer0/layer1/layer2/layer3/cloud/etc." then do... etc.
 });
