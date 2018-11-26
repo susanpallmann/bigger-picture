917946d243dc1b7fe7fd034fcce6cba09abf214d
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2018-11-26 23:29:07 +1100

    Update script.js

--- script.js
+++ script.js
@@ -45,6 +45,10 @@ function updatePage(prevScroll, currentScroll) {
         $(this).css("padding-top", offsetAmount + "px");
         $(this).children(".space").height(spaceHeight - offsetAmount);
       }
+      $(this).find(".background").css("top", spaceHeight - offsetAmount);
+      $(this).find(".midback").css("top", 0.5*(spaceHeight - offsetAmount));
+      $(this).find(".midground").css("top", -0.5*(spaceHeight - offsetAmount));
+      $(this).find(".foreground").css("top", - spaceHeight + offsetAmount);
     });
     scrollValue = currentScroll;
 }
