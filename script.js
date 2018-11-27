c286ce9e0bddd9b62483783f2cdc9de1f42bb162
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2018-11-27 11:19:55 +1100

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
             $("#top_hamburger").css("transform", "rotate(90deg)");
         }
     });
-    window.setInterval(checkScroll, 10);
+    window.setInterval(updatePage, 10);
 });
 
 function checkScroll() {
